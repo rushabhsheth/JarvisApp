fc29218f54ca06cf1e828a6a54c67c5b903c5ab9
rushabhsheth <rushabh.a.sheth@gmail.com>
2016-06-12 16:33:37 +0530

    Youtube search by keyword - test.py

--- src/Google Data API/test.py
+++ src/Google Data API/test.py
@@ -1,17 +1,60 @@
-import gdata.docs.service
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-# Create a client class which will make HTTP requests with Google Docs server.
-client = gdata.docs.service.DocsService()
-# Authenticate using your Google Docs email address and password.
-client.ClientLogin('', '')
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from oauth2client.tools import argparser
 
-# Query the server for an Atom feed containing a list of your documents.
-documents_feed = client.GetDocumentListFeed()
-# Loop through the feed and extract each document entry.
-for document_entry in documents_feed.entry:
-  # Display the title of the document on the command line.
-  print (document_entry.title.text)
-  
-  
-  
+
+# Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
+# tab of
+#   https://cloud.google.com/console
+# Please ensure that you have enabled the YouTube Data API for your project.
+DEVELOPER_KEY = "AIzaSyCwa5LRlYyskRFezRPU64DratjG3SYO-og"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+def youtube_search(options):
+  youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+    developerKey=DEVELOPER_KEY)
+
+  # Call the search.list method to retrieve results matching the specified
+  # query term.
+  search_response = youtube.search().list(
+    q=options.q,
+    part="id,snippet",
+    maxResults=options.max_results
+  ).execute()
+
+  videos = []
+  channels = []
+  playlists = []
+
+  # Add each result to the appropriate list, and then display the lists of
+  # matching videos, channels, and playlists.
+  for search_result in search_response.get("items", []):
+    if search_result["id"]["kind"] == "youtube#video":
+      videos.append("%s (%s)" % (search_result["snippet"]["title"],
+                                 search_result["id"]["videoId"]))
+    elif search_result["id"]["kind"] == "youtube#channel":
+      channels.append("%s (%s)" % (search_result["snippet"]["title"],
+                                   search_result["id"]["channelId"]))
+    elif search_result["id"]["kind"] == "youtube#playlist":
+      playlists.append("%s (%s)" % (search_result["snippet"]["title"],
+                                    search_result["id"]["playlistId"]))
+
+  print ("Videos:\n", "\n".join(videos), "\n")
+  print ("Channels:\n", "\n".join(channels), "\n")
+  print ("Playlists:\n", "\n".join(playlists), "\n")
+
+
+if __name__ == "__main__":
+  argparser.add_argument("--q", help="Search term", default="Google")
+  argparser.add_argument("--max-results", help="Max results", default=25)
+  args = argparser.parse_args()
+
+  try:
+    youtube_search(args)
+  except HttpError as e:
+    print ("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content))
   
